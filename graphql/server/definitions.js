12b3b20e2a54a158bd645437a8a864c6c405eb0e
Jacob Zuo <chopinsky@live.com>
2018-07-27 14:27:13 -0500

    adding more def

--- graphql/server/definitions.js
+++ graphql/server/definitions.js
@@ -1,6 +1,24 @@
 exports.types = `
   type Query {
     info: String!
+    feed: [Link]!
+    users: [User!]!
+    user(id: ID!): User
+  }
+
+  type Mutation {
+    createUser(name: String!): User!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
